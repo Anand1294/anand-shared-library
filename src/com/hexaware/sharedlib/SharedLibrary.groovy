148343ae73ad700d895b71d0068662cb954b92d8
Anand1294 <95375740+Anand1294@users.noreply.github.com>
2021-12-17 14:58:04 +0530

    Update SharedLibrary.groovy

--- src/com/hexaware/sharedlib/SharedLibrary.groovy
+++ src/com/hexaware/sharedlib/SharedLibrary.groovy
@@ -1,1 +1,12 @@
+package com.hexaware.sharedlib;
 
+public class SharedLibrary {
+  def steps
+  
+  public SharedLibrary(steps) {
+    this.step = steps
+  }
+  public void startBuild() {
+    steps.echo "Hello World"
+  }
+}
